faecbda4cfb659cf93888f4ea4bfbadedfc59b37
arda6 <74415046+arda6@users.noreply.github.com>
2020-12-12 23:18:06 +0300

    Add files via upload

---
+++ windowslicencer.py
@@ -0,0 +1,21 @@
+import os
+import webbrowser
+print("""
+
+
+coded by Arda 6
+Windows Licecer
+
+""")
+webbrowser.open("https://www.github.com/arda6/")
+surum = str(input("Which Windows Editions Home / Professional : ")
+if surum == 'Home':
+	os.system("slmgr /ipk TX9XD-98N7V-6WMQ6-BX7FG-H8Q9")
+	os.system("slmgr /skms kms8.msguides.com")
+	os.system("slmgr /ato")
+else surum == 'Professional':
+	os.system("slmgr /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX")
+	os.system("slmgr /skms kms8.msguides.com")
+	os.system("slmgr /ato")
+else :
+	print("Quitting...")
